9e90bad0e74dffafc27bca86fd5dfe799e330327
Semyon Voloshyn <semyonvoloshyn@MacBook-Air-Semyon.local>
2021-09-21 16:29:52 +0300

    working on custom lang

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,18 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutModule } from './about/about.module';
 import { RouterModule } from '@angular/router';
 import { TodosComponent } from './components/todos/todos.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AboutComponent } from './about/about.component';
+import { SharedModule } from './shared/shared.module';
 
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
+
   
 @NgModule({
   declarations: [
@@ -40,16 +34,7 @@ export function HttpLoaderFactory(http: HttpClient) {
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
     ]),
-    BrowserAnimationsModule,
-    MatSlideToggleModule,
-    TranslateModule.forRoot({
-      defaultLanguage: 'en',
-      loader: {
-         provide: TranslateLoader,
-         useFactory: HttpLoaderFactory,
-         deps: [HttpClient]
-       }
-    }),
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LangService } from './lang.service';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LangService],
   imports: [
     CommonModule
   ]
